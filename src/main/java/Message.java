881e8efdfd35ad4fba952e3eb44c062c9d9b1f98
kssumin <201566@jnu.ac.kr>
2022-05-09 02:18:47 +0900

    feat : 입출력 추가

---
+++ src/main/java/Message.java
@@ -0,0 +1,15 @@
+public final class Message {
+    public static final int RESTART=1;
+    public static final int EXIT=2;
+    public static final int CIPHERS=3;
+    public static final int NUMBERICALRANGE=9;
+    public static final int NUMBEROFRETRY=9;
+    public  static final String INPUT_BALL="숫자를 입력해주세요";
+    public  static final String STRIKE="스트라이크";
+    public  static final String BALL="볼";
+    public static final String BALLNET="볼넷";
+    public  static final String STRIKE_OUT="3 스트라이크 아웃";
+    public  static final String SUCCESS="3개의 숫자를 모두 맞히셨습니다! 당신의 승리입니다.";
+    public  static final String FAIL="9번의 시도 동안 맞히지 못했습니다! 당신의 패배입니다.";
+    public  static final String ASK_REPLAY="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
