a679bb0844bed05cf6520dba6fa6fbdaabc75746
kssumin <201566@jnu.ac.kr>
2022-05-09 02:07:30 +0900

    feat : 해당 판 지속여부 결정

--- src/main/java/Compare.java
+++ src/main/java/Compare.java
@@ -6,58 +6,70 @@ public class Compare {
     private int retry;
     protected boolean exit;
 
-    public Compare(){
-        this.strikeCount=0;
-        this.ball=0;
+    public Compare() {
+        this.strikeCount = 0;
+        this.ball = 0;
     }
 
     protected boolean getExit() {
         return exit;
     }
 
-    private void initScore(){
-        this.strikeCount=0;
-        this.ball=0;
+    private void initScore() {
+        this.strikeCount = 0;
+        this.ball = 0;
     }
 
-    protected void initCompare(){
-        this.strikeCount=0;
-        this.ball=0;
-        this.retry=0;
-        this.exit=false;
+    protected void initCompare() {
+        this.strikeCount = 0;
+        this.ball = 0;
+        this.retry = 0;
+        this.exit = false;
     }
 
-    public void compareNumber(ArrayList<Integer>userNumber, ArrayList<Integer>computerNumber) {
+    public void compareNumber(ArrayList<Integer> userNumber, ArrayList<Integer> computerNumber) {
         initScore();
         retry++;
-        for (int i = 0; i < Message.CIPHERS; i++)
-        {
-            validateNumber(computerNumber,userNumber.get(i),i);
+        for (int i = 0; i < Message.CIPHERS; i++) {
+            validateNumber(computerNumber, userNumber.get(i), i);
         }
         result();
     }
 
     //strike,ball 계산
-    private void validateNumber(ArrayList<Integer>computerNumber,int userNum, int userNumberIndex){
-        if(computerNumber.contains(userNum)){
-            if(computerNumber.get(userNumberIndex)==userNum){
+    private void validateNumber(ArrayList<Integer> computerNumber, int userNum, int userNumberIndex) {
+        if (computerNumber.contains(userNum)) {
+            if (computerNumber.get(userNumberIndex) == userNum) {
                 strikeCount++;
-            }
-            else
+            } else
                 ball++;
         }
     }
+
     //결과값 출력
-    public void result(){
-        if(strikeCount==3){
+    public void result() {
+        if (strikeCount == 3) {
             System.out.println(Message.STRIKE_OUT);
             System.out.println(Message.SUCCESS);
-        }else if(strikeCount==0&&ball==0){
+        } else if (strikeCount == 0 && ball == 0) {
             System.out.println(Message.BALLNET);
-        }else{
-            if(strikeCount!=0)System.out.print(strikeCount+Message.STRIKE);
-            if(ball!=0)System.out.print(ball+Message.BALL);
-        }System.out.println();
+        } else {
+            if (strikeCount != 0) System.out.print(strikeCount + Message.STRIKE);
+            if (ball != 0) System.out.print(ball + Message.BALL);
+        }
+        System.out.println();
+    }
+
+    //사용자의 다음 입력을 받을지 결정
+    public boolean isNextPlay() {
+        if (strikeCount == 3) {
+            exit = true;
+        } else if (retry == 9) {
+            exit = true;
+        } else {
+            exit = false;
+        }
+        return exit;
     }
 
 }
