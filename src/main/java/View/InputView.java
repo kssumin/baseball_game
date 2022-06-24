d3e6f2d5883617577f42b18e826309a710562071
sumin kim <88534959+kssumin@users.noreply.github.com>
2022-06-25 02:00:41 +0900

    refactor : scanner 객체 static 멤버 변수로 변경

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -3,14 +3,14 @@ package View;
 import java.util.Scanner;
 
 public class InputView {
+    static Scanner scanner = new Scanner(System.in);
+
     public static int inputRestart() {
-        Scanner scanner = new Scanner(System.in);
         int whetherToRestart = scanner.nextInt();
         return whetherToRestart;
     }
 
-    public static int inputNumbers(){
-        Scanner scanner = new Scanner(System.in);
+    public static int inputNumbers() {
         int inputUserNumbers = scanner.nextInt();
         return inputUserNumbers;
     }
